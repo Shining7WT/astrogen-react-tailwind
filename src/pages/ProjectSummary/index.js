ecf1b8e34b5b944490f6674705892712912394d3
Muhammad Arslan Razzaq <arslanrazzaq47@gmail.com>
2022-01-28 20:09:33 +0500

    toast

--- src/pages/ProjectSummary/index.js
+++ src/pages/ProjectSummary/index.js
@@ -3,27 +3,27 @@ import Button from '../../common/Button';
 import logo from '../../asset/images/Logo.png';
 import { useNavigate } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ProjectSummary = () => {
 
     const navigate = useNavigate();
-    const [showToast, setShowToast] = useState(0);
 
     const renderToast = () => {
-        return (
-            <div className='flex flex-col items-end pt-2'>
-                <div id="toast-success" class="flex items-center w-fit max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
-                    <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
-                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                    </div>
-                    <div class="ml-3 text-sm font-normal">Copied!</div>
-                </div>
-            </div>
-        )
+        toast.success('Copied!', {
+            position: "top-center",
+            autoClose: 3000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+        });
     }
 
     return (
         <>
+            <ToastContainer />
             <div className='px-0 md:px-12 lg:px-24 w-full max-w-7xl'>
                 <div className='w-full flex justify-between gap-8 py-8 px-4 md:px-0'>
                     <div className='justify-start'>
@@ -117,14 +117,13 @@ const ProjectSummary = () => {
                                 <p className='body2 !text-[#818181]'>End Time</p>
                                 <p className='body1 !text-[#565379]'>Nil</p>
                             </div>
-                            { showToast == 1 && renderToast()}
                             <div className='pt-[24px] space-y-2'>
                                 <p className='body2 !text-[#818181]'>Mint Page</p>
                                 <div className='py-3 px-4 bg-background rounded flex flex-col lg:flex-row justify-between items-end lg:items-center w-full left-[-15px] top-[14px]'>
                                     <p className='body1 !text-[#565379] w-full lg:w-auto'>
                                         Astrogen.com/Collection Name
                                     </p>
-                                    <CopyToClipboard text={'Astrogen.com/Collection Name'} onCopy={() => { setShowToast(1); setTimeout(() => setShowToast(0), 3000); }}>
+                                    <CopyToClipboard text={'Astrogen.com/Collection Name'} onCopy={() => { renderToast() }}>
                                         <Button
                                             className={'!py-1 px-4 mt-4 lg:mt-0'}
                                             type={'solid'}
@@ -134,14 +133,13 @@ const ProjectSummary = () => {
                                     </CopyToClipboard>
                                 </div>
                             </div>
-                            { showToast == 2 && renderToast()}
                             <div className='pt-[24px] pb-[90px] space-y-2'>
                                 <p className='body2 !text-[#818181]'>Embed Code</p>
                                 <div className='py-3 px-4 bg-background rounded flex flex-col lg:flex-row justify-between items-end lg:items-center w-full left-[-15px] top-[14px]'>
                                     <p className='body1 !text-[#565379] w-full lg:w-auto'>
                                         Astrogen.com/Collection Name
                                     </p>
-                                    <CopyToClipboard text={'Astrogen.com/Collection Name'} onCopy={() => { setShowToast(2); setTimeout(() => setShowToast(0), 3000); }}>
+                                    <CopyToClipboard text={'Astrogen.com/Collection Name'} onCopy={() => { renderToast() }}>
                                         <Button
                                             className={'!py-1 px-4 mt-4 lg:mt-0'}
                                             type={'solid'}
